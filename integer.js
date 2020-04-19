2450c9fa3a113ad96527d471e9ef6eb45ed4af33
FlorenceMawusi <florenceoforixyz@gmail.com>
2020-04-19 16:14:03 +0000

    '.'

---
+++ integer.js
@@ -0,0 +1,16 @@
+greaterInt();
+
+let a = 3;
+let b = 24;
+
+function greaterInt() {
+
+    if (a>b) {
+        console.log(a)
+    } else if (b>a) {
+        console.log(b)
+    } else {
+        console.log ("You entered the same numbers.")
+    } ;
+
+};
---
+++ readme.txt
@@ -0,0 +1,3 @@
+This program takes 2 integers and declares the larget one.
+There is a html front-end as well.
+PS: run using xampp.
